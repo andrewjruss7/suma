4217c4a367d6613b6a10b8dd5b6f101094036672
andrewjruss7 <andrewjruss7@gmail.com>
2024-02-07 10:19:51 -0500

    fix in the authForm

--- cypress/e2e/organism/AuthForm.ts
+++ cypress/e2e/organism/AuthForm.ts
@@ -1,5 +1,4 @@
 import { HandleForm } from "../molecules/HandleForm";
-import { Button } from "../atoms/Button";
 
 export const AuthForm = (email: string, password: string) => {
   HandleForm.fillForm({
